69d0500a6778c534a2a2612198f543c207e7bc7c
Jolop1513 <jolop1513@gmail.com>
2022-06-29 01:39:01 -0500

    commit

--- BlockJumperGame.py
+++ BlockJumperGame.py
@@ -1,3 +1,6 @@
+#Nombre: Jhostyn Javier Gavilanez Suarez 
+# APP Block Jumper Game
+
 # Se importan las librerias necesarias para trabajar con el framwork Flask
 import os
 from flask import Flask, render_template
@@ -9,12 +12,15 @@ mediante flask utilizando sus respectivas librerias.
 app = Flask(__name__)
 
 """
-Este es el paquete para llamar los templates estaticos 
+Este es el paquete para llamar los templates estaticos que estan en la carpeta
+de templates, como los css ,javscript y imagenes.
 """
 app._static_folder = os.path.abspath("templates/static/")
 
 """
-En esta parte se llama a la pagina principal del jueg
+En esta parte se llama a la pagina principal del juego para que luego 
+se ejcute de manera correcta y sastifactoria, ya que se representa mediante un
+canvas.  
 """
 @app.route("/", methods = ["GET"])
 def index():
@@ -22,7 +28,8 @@ def index():
 
 
 """
-Este es el main principal de nuestro aplicacion Game 
+Este es el main principal de nuestro aplicacion Game, se ejecuta mediante el localhost, que nos 
+va a permitir realizar dicha conexion.   
 """
 if __name__ == "__main__":
      app.run(debug=True)
